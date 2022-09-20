9d492e408c5c64421c20fd02bec5b78a4ae952ae
mindoo09 <113411423+mindoo09@users.noreply.github.com>
2022-09-20 14:29:55 +0700

    Add files via upload

---
+++ model_latihan1.php
@@ -0,0 +1,14 @@
+<?php
+class model_latihan1 extends CI_model
+{
+    //membuat variable untuk menampung nilai
+    public $nilai1, $nilai2, $hasil;
+    //method penjumlahan
+    public function jumlah($n1 = null, $n2 = null)
+    {
+        $this->nilai1 = $nl;
+        $this->nilai2 = $n2;
+        $this->hasil = $this->niali1 + $this->nilai2;
+        return $this->hasil;
+    }
+}
---
+++ view-latihan.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>latihan1</title>
+</head>
+
+<body>
+    halo bestie.. Yuk sholattt!!!!<br>
+    nilai 1 = <?= $nilai1; ?>
+    nilai 2 = </= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+$niali1 . " + " > $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
